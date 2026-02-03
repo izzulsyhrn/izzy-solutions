271c4dc7fc6c8d20ee083056fb32f8d0c23501d0
izzulsyhrn <izzulsyhrn@gmail.com>
2026-02-04 01:48:58 +0800

    Change repository name for GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'vfl-valorant'; // Auto ambik nama repo dari GitHub Actions, atau hardcode nama repo kau (contoh: 'vfl-valorant')
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'izzy-solutions'; // Auto ambik nama repo dari GitHub Actions, atau hardcode nama repo kau (contoh: 'vfl-valorant')
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -7,9 +7,9 @@ const nextConfig = {
   // Wajib untuk GitHub Pages / static hosting
   output: 'export',  // Ini buat static HTML export → folder 'out/'
 
-  // Untuk subpath GitHub Pages (contoh: /vfl-valorant)
-  basePath: isProd ? `/${repoName}` : '',          // Kosongkan masa dev (localhost)
-  assetPrefix: isProd ? `/${repoName}/` : '',      // Tambah trailing slash penting!
+  // Untuk subpath GitHub Pages 
+  basePath: isProd ? `/${izzy-solutions}` : '',          // Kosongkan masa dev (localhost)
+  assetPrefix: isProd ? `/${izzy-solutions}/` : '',      // Tambah trailing slash penting!
 
   // Images: Wajib unoptimized sebab GitHub Pages tak support Next Image optimization
   images: {
